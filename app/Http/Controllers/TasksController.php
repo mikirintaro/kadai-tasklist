15570be33b9e0a686ac38e3a1e0b9f9faf924d51
mikirintaro <k.s.gandhi@nifty.com>
2020-10-13 12:08:45 +0000

    status

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -42,8 +42,14 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        // バリデーション
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+        ]);
         // メッセージを作成
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -91,8 +97,14 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        // バリデーション
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+        ]);
         // メッセージを作成
         $task = Task::findOrFail($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
